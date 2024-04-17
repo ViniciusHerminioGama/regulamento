e32a13ce7ca32ba7c8d723aa216ca20801fc890e
Vinicius Herminio <viniciusherminio1@outlook.com>
2024-04-17 14:23:43 -0300

    regulamento individual

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Regulamento Campanha</h1>
+      <h3 className='title'>Indique um amigo e ganhe desconto</h3>
+      <div className="content">
+        <p><strong>Data:</strong> 25.03.2024</p>
+        <p><strong>Promoção:</strong> Indique amigo e ganhe desconto em sua mensalidade.</p>
+        <p>1- A campanha de indicação só é válida para clientes pessoa física ativos e adimplentes.</p>
+        <p>2- A promoção só é válida a partir do dia 01 de abril de 2024, por tempo indeterminado, até que a empresa comunique o encerramento da mesma;</p>
+        <p>3- O assinante Oxente.net que indicar um amigo e este aderir aos nossos serviços no prazo de até 30 dias após a indicação, receberá desconto de 50% (o valor do desconto é limitado a R$50,00) do valor da sua mensalidade. O benefício será concedido na primeira mensalidade posterior ao primeiro pagamento feito pelo amigo indicado já como assinante.</p>
+        <p>4- O número de indicações é ilimitada, porém os descontos são limitados ao valor concedido por 1 (uma) indicação por mês, com limite de R$50,00;</p>
+        <p>5- O desconto gerado é para o titular do contrato, indicado no formulário, não podendo ser transferido para outro cliente;</p>
+        <p>6- Não é permitido ressarcimento em valor (dinheiro) ao cliente, o benefício é apenas desconto em mensalidades e limitado ao serviço de conexão à internet;</p>
+      </div>
     </div>
   );
 }
